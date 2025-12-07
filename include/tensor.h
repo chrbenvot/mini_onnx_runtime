69f8f40bf5c143612077f2ba75837b50d866c2c3
chrbenvot <hammamhiri@gmail.com>
2025-12-07 18:26:55 +0100

    Start project

---
+++ include/tensor.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <vector>
+#include <iostream>
+#include <numeric>
+#include <cassert>
+
+
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "third_party/onnx/onnx.pb.h"
+
+int main(){
+    std::cout<<"Hello world!";
+    return 0;
+}
